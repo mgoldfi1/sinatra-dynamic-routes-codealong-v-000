65d3f07f09c1d49500d0abd8c56b0660fd1ec5ae
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-01 16:53:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -22,7 +22,7 @@ class App < Sinatra::Base
   get "/multiply/:num_1/:num_2" do
     @num_1 = params[:num_1]
     @num_2 = params[:num_2]
-    binding.pry
+    # binding.pry
     "#{@num_1.to_i*@num_2.to_i}"
 
   end
